8f27a3c182c23049692e1bbff961cb4d3fa8a8db
ahmed-ayman-bastawisy <ahmed.ayman.bastawisy@gmail.com>
2021-05-14 19:13:32 +0200

    add user

---
+++ UserAuthMVC/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserAuthMVC.Models;
+
+namespace UserAuthMVC.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            User dbUser = new User
+            {
+                FName = user.FName,
+                LName = user.LName,
+                Age = user.Age
+            };
+
+            return View("GetUser", dbUser);
+        }
+
+        [HttpGet]
+        public IActionResult GetUser(User user)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ UserAuthMVC/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAuthMVC.Models
+{
+    public class User
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public int Age { get; set; }
+        public bool LegalAge { get { return Age > 18; } }
+    }
+}
