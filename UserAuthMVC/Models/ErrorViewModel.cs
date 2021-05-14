76e4e8397bc3f31d84bafddfd3ced1f8e5a29262
ahmed-ayman-bastawisy <ahmed.ayman.bastawisy@gmail.com>
2021-05-14 18:05:58 +0200

    Add project files.

---
+++ UserAuthMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UserAuthMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
